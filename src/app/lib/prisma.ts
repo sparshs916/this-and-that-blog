885e465c7ce1e2f7e2a79dbd96965d7a77fea0b6
sparshs916 <sparshs916@gmail.com>
2025-05-10 20:26:45 -0700

    npm build error fixing again

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -1,21 +1,21 @@
-import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaClient as OriginalPrismaClient } from "@/generated/prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
-// Define a global variable to hold the Prisma client instance
+const prismaClientSingleton = () => {
+  return new OriginalPrismaClient().$extends(withAccelerate());
+};
+
+type PrismaClientExtended = ReturnType<typeof prismaClientSingleton>;
+
 declare global {
   // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
+  var prismaGlobalInstance: PrismaClientExtended | undefined;
 }
 
-// Initialize Prisma Client with Accelerate extension
-const prisma =
-  global.prisma ||
-  new PrismaClient().$extends(withAccelerate());
-
-// In development, assign the Prisma client to the global variable
-// This prevents creating a new client on every hot reload
-if (process.env.NODE_ENV !== "production") {
-  global.prisma = prisma;
-}
+const prisma = globalThis.prismaGlobalInstance ?? prismaClientSingleton();
 
 export default prisma;
+
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prismaGlobalInstance = prisma;
+}
